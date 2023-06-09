ff6ebebbfec6badaa1118480bc226543b96ad997
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-06-09 09:45:46 +0100

    Display api status

--- README.md
+++ README.md
@@ -22,10 +22,22 @@ The console is the fourth segment of the AirBnB project at Holberton School that
 This project is interpreted/tested on Ubuntu 14.04 LTS using python3 (version 3.4.3)
 
 ## Installation
-* Clone this repository: `git clone "https://github.com/alexaorrico/AirBnB_clone.git"`
-* Access AirBnb directory: `cd AirBnB_clone`
-* Run hbnb(interactively): `./console` and enter command
-* Run hbnb(non-interactively): `echo "<command>" | ./console.py`
+* Clone this repository: `git clone "https://github.com/sixtusagbo/AirBnB_clone_v4.git"`
+* Access AirBnb directory: `cd AirBnB_clone_v4`
+* Run hbnb console(interactively): `./console` and enter command
+* Run hbnb console(non-interactively): `echo "<command>" | ./console.py`
+* Run the flask web app (with db):
+    ```bash
+       HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd
+       HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db
+       python3 -m web_dynamic.2-hbnb
+    ```
+* Run the flask api (with db):
+    ```bash
+       HBNB_API_PORT=5001 HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd
+       HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db
+       python3 -m api.v1.app
+    ```
 
 ## File Descriptions
 [console.py](console.py) - the console contains the entry point of the command interpreter. 
---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,43 @@
+$(function () {
+    let amenitiesChecked = {};
+
+    // Listen for changes on each input checkbox tag
+    $(".amenities input:checkbox").change(function (event) {
+        if ($(this).is(":checked")) {
+            // store the Amenity ID in the list
+            amenitiesChecked[$(this).data("id")] = $(this).data("name");
+            updateCheckedAmenitiesInterface();
+        } else {
+            // remove the Amenity ID from the list
+            delete amenitiesChecked[$(this).data("id")];
+            updateCheckedAmenitiesInterface();
+        }
+    });
+
+    // update the h4 tag inside div Amenities with list of Amenities checked
+    const updateCheckedAmenitiesInterface = () => {
+        const length = Object.keys(amenitiesChecked).length;
+        let content = "", i = 0;
+
+        for (amenity of Object.values(amenitiesChecked)) {
+            const last = i === length - 1;
+            content += amenity;
+            if (!last) {
+               content += ", ";
+            }
+            i++;
+        }
+
+        $(".amenities h4").text(content);
+    }
+
+    $.get('http://127.0.0.1:5001/api/v1/status/',
+        function (data, textStatus, jqXHR) {
+            if (data.status === 'OK') {
+                $("div#api_status").addClass('available');
+            } else {
+                $("div#api_status").removeClass('available');
+            }
+        }
+    );
+});
